59d505f8bd175df63717c0b531b7526aeaba6fab
rishizsinha <rishi.z.sinha@gmail.com>
2015-08-21 00:39:51 -0700

    testing php

--- js/stockexplorerdata.js
+++ js/stockexplorerdata.js
@@ -1,49 +1,30 @@
-function symbLookup() {
+function xDomData(url) {
+	var proxy = '../php/ba-simple-proxy.php',
+		url = proxy + "?" + url.serialize();
+
+	if ( /mode=native/.test(url)) {
+		$.get(url, function(data){
+			return data.contents;
+		});
+	} else {
+		$.getJSON(url, function(data){
+			return JSON.stringify(data, null, 2);
+		});
+	}
+}
+
+$.ajaxSetup({ cache: false });
+
+$("#stockCheck").click(function() {
 	var data = [];
 	var symbol = "http://www.google.com/finance/historical?output=csv&q="+$("#stockInput").val();
 	console.log(symbol);
-	data = CORS(symbol);
+	data = xDomData(symbol);
 	// d3.csv(symbol,function(csv){
 	// 	data = csv;
 	// });
 	console.log(data);
-}
-
-function CORS(url) {
-	function createCORSRequest(method, url) {
-	  var xhr = new XMLHttpRequest();
-	  if ("withCredentials" in xhr) {
-
-	    // Check if the XMLHttpRequest object has a "withCredentials" property.
-	    // "withCredentials" only exists on XMLHTTPRequest2 objects.
-	    xhr.open(method, url, true);
-
-	  } else if (typeof XDomainRequest != "undefined") {
-
-	    // Otherwise, check if XDomainRequest.
-	    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-	    xhr = new XDomainRequest();
-	    xhr.open(method, url);
-
-	  } else {
-
-	    // Otherwise, CORS is not supported by the browser.
-	    xhr = null;
-
-	  }
-	  return xhr;
-	}
-
-	var xhr = createCORSRequest('GET', url);
-	if (!xhr) {
-	  throw new Error('CORS not supported');
-	}
-
-	return xhr;
-}
-
-
-$("#stockCheck").click(function() {
-	symbLookup();
 });
+
+
 
