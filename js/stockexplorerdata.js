28d859b5bdda18768e3a06b681a9daccfad280dc
rishizsinha <rishi.z.sinha@gmail.com>
2015-08-22 01:08:18 -0700

    successfully extracting stock data contents

--- js/stockexplorerdata.js
+++ js/stockexplorerdata.js
@@ -1,31 +1,38 @@
-function xDomData(u) {
-	var proxy = 'php/ba-simple-proxy.php',
-		url = proxy + "?url=" + u;
 
-	console.log(url);
+function xDomData(u) {
+	console.log("--- CROSS DOMAIN CALL ---")
+	var proxy = '../php/ba-simple-proxy.php',
+		url = proxy + "?url=" + u;
+	console.log("url called: " + url);
 	var d;
-	if ( /mode=native/.test(url)) {
-		console.log("here");
-		d = $.get(url, function(data){
-			return data.responseText;
-		});
-	} else {
-		console.log("there");
-		d = $.getJSON(url, function(data){
-			return data;//JSON.stringify(data, null, 2);
-		});
-	}
+	// if ( /mode=native/.test(url)) {
+	// 	console.log("here");
+	// 	d = $.get(url, function(data){
+	// 		return data.responseText;
+	// 	});
+	// } else {
+	$.ajax({
+		url:url,
+		type:"POST",
+		async:false}).done( function(data){
+		//t = JSON.stringify(data, null, 2);
+		$("#test").text(JSON.stringify(data.contents, null, 2));
+		d = JSON.stringify(data.contents, null, 2)
+	});
+	// }
 	return d;
 }
 
 $.ajaxSetup({ cache: false });
 
 $("#stockCheck").click(function() {
-	var data = [];
-	var symbol = "http://www.google.com/finance/historical?output=csv&q="+$("#stockInput").val();
-	console.log(symbol);
-	data = xDomData(symbol);
+	var symbol = "http%3A%2F%2Fwww.google.com%2Ffinance%2Fhistorical%3Foutput%3Dcsv%26q%3D"+$("#stockInput").val()+"&full_headers=1&full_status=1";
+	var data = xDomData(symbol);
 	console.log(data);
+	var jsondata = d3.csv.parse(data);
+	console.log(d3.csv.parse(data));
+	console.log(d3.csv.parse("Year,Make,Model,Length\n1997,Ford,E350,2.34\n2000,Mercury,Cougar,2.3\n"));
+
 });
 
 
